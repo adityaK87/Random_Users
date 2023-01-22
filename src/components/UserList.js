5d3edeca58c78e77aab620cbfeb21e6840d21191
Aditya kumar <adityapal8763244@gmail.com>
2023-01-22 16:54:49 +0530

    add search feature

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import User from './User';
 
 const UserList = ({ users }) => {
+    const [searchField, setSearchField] = useState("")
+
+    const searchUserData = users.filter(
+        (person) => {
+            return (
+                (person.name.first.toLowerCase().includes(searchField.toLowerCase()))
+            )
+        }
+    )
+
+    const handleSearch = (event) => {
+        event.preventDefault()
+        setSearchField(event.target.value)
+    }
+
     return (
-        <div style={{
-            margin: ' 0px auto',
-            display: 'flex',
-            flexDirection: "column",
-            alignItems: "center"
-        }}>
-            {users?.map((user) => { return <User key={user.login.uuid} user={{ ...user }} /*user={user}*/ /> })}
-        </div>
+        <>
+            <div style={{
+                margin: ' 0px auto',
+                display: 'flex',
+                flexDirection: "column",
+                alignItems: "center"
+            }}>
+                <input type="text" onChange={handleSearch} placeholder="Search Users" style={{
+                    padding: '4px',
+                    marginTop: '12px',
+                    border: '2px solid #000',
+                    borderRadius: '4px',
+                    color: '#000',
+                }} />
+                {searchUserData?.map((user) => { return <User key={user.login.uuid} user={{ ...user }} /*user={user}*/ /> })}
+            </div>
+        </>
     )
 }
 
